b3d496edd2370d0be3f6b890269b663409f5da1e
Carlos Rodas <crods57@gmail.com>
2020-07-03 15:24:28 -0500

    Headers and status codes

--- 01-http/introduccion/src/http/http-juego.controller.ts
+++ 01-http/introduccion/src/http/http-juego.controller.ts
@@ -1,11 +1,27 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Delete, Get, Header, HttpCode, Post} from '@nestjs/common';
 
 // http://localhost:3001/juegos-http
 @Controller('juegos-http')
 export class HttpJuegoController {
 
     @Get('hola')
-    hola(){
-        return 'Hola mundo! >:)';
+    @HttpCode(201)
+    holaGet(){
+        return 'Hola GET! >:)';
     }
+
+    @Post('hola')
+    @HttpCode(202)
+    holaPost(){
+        return 'Hola POST! >:)';
+    }
+
+    @Delete('hola')
+    @HttpCode(204)
+    @Header('Cache-control', 'none')
+    @Header('EPN', 'probando las cosas')
+    holaDelete(){
+        return 'Hola DELETE! >:)';
+    }
+
 }
