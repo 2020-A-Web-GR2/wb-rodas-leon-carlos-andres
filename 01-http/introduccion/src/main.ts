b8529572852e4d869487a1816e516152555766b1
Carlos Rodas <crods57@gmail.com>
2020-07-10 15:42:02 -0500

    Validaciones

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // npm run start:dev -- para iniciar el servidor
   // le cambiamos a 3001 solo para probar
+  app.use(cookieParser());
   await app.listen(3001);
 }
 bootstrap();
