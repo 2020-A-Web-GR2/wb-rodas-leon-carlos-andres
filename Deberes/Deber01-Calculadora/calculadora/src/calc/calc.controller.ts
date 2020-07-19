6479cec39ea0566475455f5f45b270d4b07ca54b
Carlos Rodas <crods57@gmail.com>
2020-07-19 17:16:14 -0500

    Creación Deber01

---
+++ Deberes/Deber01-Calculadora/calculadora/src/calc/calc.controller.ts
@@ -0,0 +1,20 @@
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Header,
+    HttpCode,
+    Param,
+    Post,
+    Query, Req, Res
+} from '@nestjs/common';
+// import {MascotaCreateDto} from "./dto/mascota.create-dto";
+// import {validate, ValidationError} from "class-validator";
+
+// http://localhost:3001/juegos-http
+@Controller('calc')
+export class CalcController {
+
+}
