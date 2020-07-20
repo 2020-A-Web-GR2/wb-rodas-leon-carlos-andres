7b2dfd6cad13f2d68b893ef3a6670a98d8dd3662
Carlos Rodas <crods57@gmail.com>
2020-07-19 19:46:43 -0500

    Deber01 terminado

--- Deberes/Deber01-Calculadora/calculadora/src/calc/calc.controller.ts
+++ Deberes/Deber01-Calculadora/calculadora/src/calc/calc.controller.ts
@@ -1,9 +1,9 @@
 import {
-    BadRequestException, Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Query, Req, Res,
+    BadRequestException, Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Query, Req, Res, Headers
 } from '@nestjs/common';
-import { validate, ValidationError } from 'class-validator';
-import { SumCalcDto } from './dto/sum.calc-dto';
-import { DividirCalcDto } from './dto/dividir.calc-dto';
+// import { validate, ValidationError } from 'class-validator';
+// import { SumCalcDto } from './dto/sum.calc-dto';
+// import { DividirCalcDto } from './dto/dividir.calc-dto';
 
 // http://localhost:3002/calc
 @Controller('calc')
@@ -33,7 +33,7 @@ export class CalcController {
         }
     }
 
-    // Sumar n1 + n2
+    // Sumar
     // http://localhost:3002/calc/sumar/7
     @Get('sumar/:n2')
     @HttpCode(200)
@@ -55,8 +55,8 @@ export class CalcController {
         }
     }
 
-    // Restar n1 - n2
-    //
+    // Restar
+    // http://localhost:3002/calc/restar
     @Put('restar')
     @HttpCode(201)
     restarPut(
@@ -70,6 +70,52 @@ export class CalcController {
             const validarN1yN2 = body.n1 && query.n2 && !isNaN(body.n1) && !isNaN(query.n2)
             if (validarN1yN2) {
                 return body.n1 - Number(query.n2)
+            } else {
+                throw new BadRequestException('Datos incorrectos')
+            }
+        } else {
+            return "No está registrado"
+        }
+    }
+
+    // Multiplicación
+    // http://localhost:3002/calc/multiplicar
+    @Delete('multiplicar')
+    @HttpCode(200)
+    multiplicarDelete(
+        @Headers() headers,
+        @Body() body,
+        @Req() req
+    ){
+        const validarNombreUsuario = req.cookies["nombre usuario"]
+
+        if (validarNombreUsuario){
+            const validarN1yN2 = headers["n1"] && body.n2 && !isNaN(headers["n1"])  && !isNaN(body.n2)
+            if(validarN1yN2){
+                return Number(headers["n1"]) * Number(body.n2)
+            }else{
+                throw new BadRequestException('Datos incorrectos')
+            }
+        } else {
+            return "No está registrado"
+        }
+    }
+
+    // Devisión
+    // http://localhost:3002/calc/dividir/20
+    @Post('dividir/:n1')
+    @HttpCode(201)
+    dividirDelete(
+        @Param() param,
+        @Headers() headers,
+        @Req() req
+    ){
+        const validarNombreUsuario = req.cookies["nombre usuario"]
+
+        if (validarNombreUsuario) {
+            const validarN1yN2 = param.n1 && headers["n2"] && !isNaN(param.n1) && !isNaN(headers["n2"])
+            if (validarN1yN2 && (Number(headers["n2"]) != 0)) {
+                return Number(param.n1) / Number(headers["n2"])
             } else {
                 throw new BadRequestException('Datos incorrectos')
             }
