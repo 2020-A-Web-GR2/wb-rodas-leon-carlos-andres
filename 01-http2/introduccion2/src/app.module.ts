1c640c14741df6261b18969c0ead697b81f3cfef
Carlos Rodas <crods57@gmail.com>
2020-07-24 16:23:53 -0500

    Conexión con MySQL terminada

--- 01-http2/introduccion2/src/app.module.ts
+++ 01-http2/introduccion2/src/app.module.ts
@@ -1,38 +1,40 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 import {HttpJuegoModule} from './http/http-juego.module';
 import {UsuarioModule} from './usuario/usuario.module';
-import {UsuarioService} from "./usuario/usuario.service";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {UsuarioEntity} from "./usuario/usuario.entity";
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UsuarioEntity} from './usuario/usuario.entity';
 
 @Module({
   imports: [
     // Aqui otros modulos
     HttpJuegoModule,
     UsuarioModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'ejemplo',
-      entities: [
-          UsuarioEntity,
-      ],
-      synchronize: true,
-    }),
+    TypeOrmModule
+        .forRoot({
+          name: 'default', // nombre conexión
+          type: 'mysql', // mysql postgres
+          host: 'localhost', // ip
+          port: 3306, // puerto
+          username: 'root', // usuario
+          password: '', // password
+          database: 'ejemplo', //  Base de Datos
+          entities: [  // TODAS LAS ENTIDADES
+            UsuarioEntity
+          ],
+          synchronize: true, // Actualiza el esquema de la base de datos
+          dropSchema: false, // Eliminar Datos y el Esquema de base de datos
+        }),
   ],
   controllers: [
     // Controladores APP MODULE
-    AppController,
+    AppController
   ],
   providers: [
     // Servicios APP MODULE
-    AppService,
-    UsuarioService
+    AppService
   ],
 })
-export class AppModule {}
+export class AppModule {
+}
