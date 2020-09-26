cad97b40060e5738e9ccffac1aa5d1a75d3d86af
Carlos Rodas <crods57@gmail.com>
2020-09-26 15:45:07 -0500

    Ya funciona el log in

--- Deberes/crud-app/src/app.module.ts
+++ Deberes/crud-app/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+
+
 @Module({
   imports: [
       ContactsModule,
--- Deberes/crud-app/src/main.ts
+++ Deberes/crud-app/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const session = require('express-session')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const FileStore = require('session-file-store')(session)
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as any;
+
+  const secret = 'pokemon'
+  app.set('view engine', 'ejs');
+  app.use(express.static('publico'))
+  app.use(
+      session({
+        name: 'server-session-id',
+        secret: secret,
+        resave: true,
+        saveUninitialized: true,
+        cookie: {secure: false},
+        store: new FileStore(),
+      }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
